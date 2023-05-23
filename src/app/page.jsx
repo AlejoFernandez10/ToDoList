9f8d56e188332959d205f15e73a483cba775f03d
AlejoFernandez10 <alejofernandez1239@gmail.com>
2023-05-23 17:43:26 -0300

    Initializing repository for toDo list project, hero started

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Home from '@/components/Home'
+import TasksContainer from '@/components/TasksContainer'
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+
+        <Home />
+          
+        <TasksContainer />
+
+    </main>
+  )
+}
