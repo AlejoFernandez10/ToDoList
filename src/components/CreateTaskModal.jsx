8fad95c4d739b48c01aab7049fd97caa2dfdbeb6
AlejoFernandez10 <alejofernandez1239@gmail.com>
2023-05-25 10:22:30 -0300

    Logic finished

--- src/components/CreateTaskModal.jsx
+++ src/components/CreateTaskModal.jsx
@@ -1,19 +1,31 @@
 'use client'
 import { Fragment, useEffect, useRef, useState, useContext } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-
+import { Dialog, Transition, Menu } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Context } from '@/context/TasksContext'
+import { XMarkIcon,FlagIcon } from '@heroicons/react/24/outline'
+import { space } from 'postcss/lib/list'
 
 
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
+const priorities =[
+  'Priority 1',
+  'Priority 2',
+  'Priority 3',
+]
 
 export default function CreateTaskModal( {state, activeDay }) {
 
 
   const [open, setOpen] = useState(false)
   const [tasks, setTasks] = useContext(Context)
-
+  const [priority, setPrioriy] = useState('Priority')  
   
-  console.log(tasks)
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
     
@@ -34,9 +46,15 @@ export default function CreateTaskModal( {state, activeDay }) {
     const data = {id,activeDay, title, description}
     
     
-    if(tasks[activeDay]){
-     setTasks( tasks[activeDay].push(data))
-    }
+    setTasks((current)=>{
+
+      const newTaks = {...current}
+      if(newTaks[activeDay]){
+        newTaks[activeDay].push(data)
+      }
+
+      return newTaks
+    })
   }
 
   const cancelButtonRef = useRef(null)
@@ -57,39 +75,39 @@ export default function CreateTaskModal( {state, activeDay }) {
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full  justify-center p-4 text-center items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterFrom="opacity-0 translate-y-0 sm:scale-95"
               enterTo="opacity-100 translate-y-0 sm:scale-100"
               leave="ease-in duration-200"
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              leaveTo="opacity-0  translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-[#161d30]  text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <form action="">
+              <Dialog.Panel className=" h-auto   rounded-lg bg-[#161d30]  text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                
 
-                <div className="bg-[#161d30] px-5 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div className="bg-[#161d30] relative px-5 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="">
                     <div className="mx-auto flex h-12 w-20 flex-shrink-0 items-center justify-center rounded-full   text-yellow-400">
                       New Task
                     </div>
                     <div className="mt-3  sm:ml-4 sm:mt-0 ">
                       
-                      <div className="mt-2 flex flex-col gap-3">
+                      <div className="mt-2 flex flex-col gap-3 sm:gap-5">
 
                         <div>
 
-                          <label htmlFor="title" className='text-sm text-gray-100'>Title:</label>
-                          <input type="text" name='title' onChange={(e)=> setTitle(e.target.value)}  className="text-sm  pl-3 w-full min-w-[250px] text-gray-300 border border-gray-400 rounded-[5px] py-1.5 bg-transparent"/>   
+                          
+                          <input type="text" name='title' onChange={(e)=> setTitle(e.target.value)} placeholder='Title'  className="text-base   pl-3 w-full min-w-[250px] text-gray-300 border border-gray-400 sm:border-none rounded-[5px] py-1.5 bg-transparent"/>   
                         
                         </div>
 
                         <div>
 
-                          <label htmlFor="desc" className='text-sm text-gray-100'>Description: </label>
-                          <textarea name='desc' onChange={(e)=> setDescription(e.target.value)} className="text-sm w-full min-w-[250px] pl-3  text-gray-300 border border-gray-400 rounded-md py-1.5 bg-transparent"/>
+                          
+                          <textarea name='desc' onChange={(e)=> setDescription(e.target.value)} placeholder='Description' className="text-sm w-full min-w-[250px] pl-3  text-gray-300 border border-gray-400 sm:border-none rounded-md py-1.5 bg-transparent"/>
                         
                         </div>
 
@@ -97,24 +115,84 @@ export default function CreateTaskModal( {state, activeDay }) {
                     </div>
                   </div>
                 </div>
-                <div className="bg-[#161d30] px-4 py-3 flex  sm:px-6">
+                <div className="bg-[#161d30]   px-4 py-3 flex w-full justify-between sm:px-6">
+
+                <Menu as="div" className=" relative inline-block text-left">
+                    <div>
+                      <Menu.Button className=" inline-flex w-full justify-center gap-x-1.5 rounded-md bg-gray-900 px-3 py-2 text-[13px] font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-700 ">
+                        
+
+                        {priority !== 'Priority' ?
+                        <>
+                          <span>{priority} </span>
+                        <FlagIcon className={`h-5 w-5 ${priority === 'P1' && 'text-red-500'} ${priority === 'P2' && 'text-yellow-500'} ${priority === 'P3' && 'text-green-500'} `} />
+                        </>
+                        :
+                        priority
+                        }
+
+
+                        <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                      </Menu.Button>
+                    </div>
+
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items   className=" absolute left-0 mt-2 w-36  origin-top-right rounded-md bg-gray-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none border border-gray-700">
+                        <div className="py-1">
+
+
+                          {priorities.map((pri)=>(
+
+                          <Menu.Item key={pri}>
+                            {({ active }) => (
+                              <button
+                                onClick={()=> setPrioriy(pri.slice(0,1) + pri.slice(9,10))}
+                                className={classNames(
+                                  active ? ' text-gray-200' : 'text-gray-200',
+                                  'flex gap-2 px-4 py-2 text-[13px] '
+                                )}
+                              >
+                                <FlagIcon className={`h-5 w-5 ${pri === 'Priority 1' && 'text-red-500'} ${pri === 'Priority 2' && 'text-yellow-500'} ${pri === 'Priority 3' && 'text-green-500'} `} />
+
+                               {pri}
+                              </button>
+                            )}
+                          </Menu.Item>
+                          ))}                        
+                         
+                          
+                        </div>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center items-center rounded-md bg-white ring-gray-300 hover:bg-gray-50  px-3 py-2.5 text-sm font-semibold text-gray-900 shadow-sm  sm:ml-3 sm:w-auto"
+                    className="  justify-center items-center  w-auto rounded-md bg-white ring-gray-300 hover:bg-gray-50  px-3 py-2.5 text-sm font-semibold text-gray-900 shadow-sm  sm:ml-3 sm:w-auto"
                     onClick={() => createTask() & setOpen(false)}
                   >
                     Add
                   </button>
+
+                </div>
                   <button
                     type="button"
-                    className="inline-flex w-full items-center justify-center rounded-md  px-3 py-2.5 text-sm font-semibold text-white shadow-sm   sm:mt-0 sm:w-auto"
+                    className="  items-center justify-center rounded-md   text-sm font-semibold text-white shadow-sm  absolute top-5 right-2   "
                     onClick={() => setOpen(false)}
                     ref={cancelButtonRef}
                   >
-                    Cancel
+                    <XMarkIcon className='text-sm w-8 h-8   right-0' />
                   </button>
-                </div>
-                </form>
+
+               
               </Dialog.Panel>
             </Transition.Child>
           </div>
