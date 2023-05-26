ef4653c8cd0ec879a67b60923cfdd269f48b3e5c
AlejoFernandez10 <alejofernandez1239@gmail.com>
2023-05-26 17:40:36 -0300

    Tasks added to localStorage, responsive design improved, improvements in logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Disclosure} from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Dashboard', href: '#addtasks', current: true },
--- src/context/TasksContext.jsx
+++ src/context/TasksContext.jsx
@@ -6,16 +6,29 @@ export const Context = createContext({});
 
 const TasksContext = ({ children }) => {
   const [tasks, setTasks] = useState(() => {
-    const storedTasks = typeof window !== 'undefined' ? localStorage.getItem('tasks') : {};
-    return storedTasks ? JSON.parse(storedTasks) : {
-      Monday: [],
-      Tuesday: [],
-      Wednesday: [],
-      Thursday: [],
-      Friday: [],
-      Saturday: [],
-      Sunday: [],
-    };
+    const storedTasks = typeof window !== 'undefined' ? localStorage.getItem('tasks') : null;
+    try {
+      return storedTasks ? JSON.parse(storedTasks) : {
+        Monday: [],
+        Tuesday: [],
+        Wednesday: [],
+        Thursday: [],
+        Friday: [],
+        Saturday: [],
+        Sunday: [],
+      };
+    } catch (error) {
+      console.error('Error parsing stored tasks:', error);
+      return {
+        Monday: [],
+        Tuesday: [],
+        Wednesday: [],
+        Thursday: [],
+        Friday: [],
+        Saturday: [],
+        Sunday: [],
+      };
+    }
   });
 
   useEffect(() => {
